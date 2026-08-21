024e74394579a2cf27b85c45dea79d86edc258ec
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 14:51:37 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,6 +1,7 @@
--- Name:
--- Register Number:
-
--- Write your CREATE TABLE statement below
-
-
+CREATE TABLE Student (
+    StudentID INT(5) PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
