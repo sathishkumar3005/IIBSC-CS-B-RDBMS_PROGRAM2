ec6b817720b30a6d17fb8e989ccbf8e8ac71d5a4
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 12:42:40 +0530

    Create student_solution.sql

---
+++ student_solution.sql
@@ -0,0 +1,18 @@
+-- =========================================
+-- SQL Assignment: Student Table
+-- Name:
+-- Register Number:
+-- =========================================
+
+-- Create a table named Student with the following fields:
+--
+-- StudentID      INT(5)       PRIMARY KEY
+-- StudentName    VARCHAR(20)  NOT NULL
+-- DOB            DATE         NOT NULL
+-- Gender         VARCHAR(10)  NOT NULL
+-- DepartmentID   INT(5)       NOT NULL
+--
+-- Add an appropriate UNIQUE constraint.
+
+-- Write your SQL statement below:
+
