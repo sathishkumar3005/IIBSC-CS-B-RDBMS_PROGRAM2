a461299d7268c6771e63bb29f4057bd9068fef4b
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 12:52:16 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,7 +1,1 @@
-CREATE TABLE Student (
-    StudentID INT PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL UNIQUE,
-    DOB DATE NOT NULL,
-    Gender VARCHAR(10) NOT NULL,
-    DepartmentID INT NOT NULL
-);
+
