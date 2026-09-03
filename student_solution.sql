97c5182f5b7cc6e275ef1ea5af0ff90b0b6d6466
SATHISHKUMAR P <sathishkumar99611@gmail.com>
2026-09-03 09:45:29 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,11 @@
-
+create table student80(
+student80id int(5) primary key,
+student80name varchar(20)not null,
+DOB date default null,
+gender varchar(10),
+info80id int(5),
+CONSTRAINT UQ_student80name UNIQUE(student80name),
+CONSTRAINT FK_info80id FOREIGN KEY(info80id)
+references info80(info80id)
+);
+desc student80;
